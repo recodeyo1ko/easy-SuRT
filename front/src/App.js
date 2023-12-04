268c8330f91fee2c86410a768154057e29c6edeb
recodeyo1ko <kuronekoryohei@gmail.com>
2023-12-04 17:12:20 +0900

    レイアウト修正

--- front/src/App.js
+++ front/src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   const generateGrid = () => {
     const totalCells = 60;
-    const numberOfCircles = Math.floor(Math.random() * (30 - 15 + 1)) + 15;
+    const numberOfCircles = Math.floor(Math.random() * (25 - 15 + 1)) + 15;
     let newCirclePositions = [];
     for (let i = 0; i < totalCells; i++) {
       if (newCirclePositions.length < numberOfCircles) {
